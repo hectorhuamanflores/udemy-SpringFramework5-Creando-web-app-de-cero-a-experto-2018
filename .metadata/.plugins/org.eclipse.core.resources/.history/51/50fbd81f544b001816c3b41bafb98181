1ebe96032918a12ce39637f2ce63cbb351fde978
hector <hector.10.fisi@gmail.com>
2018-04-29 22:21:51 -0500

    Seccion 3 - Spring MVC & Data JPA Desarrollando una aplicación desde cero - crud completo

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/51/50fbd81f544b001816c3b41bafb98181
@@ -0,0 +1,5 @@
+package com.example.demo.models.dao;
+
+public interface IClienteDao {
+
+}
---
+++ spring-boot-data-jpa/src/main/java/com/example/demo/models/dao/IClienteDao.java
@@ -0,0 +1,16 @@
+package com.example.demo.models.dao;
+
+import java.util.List;
+
+import com.example.demo.models.entity.Cliente;
+
+public interface IClienteDao {
+    
+	public List<Cliente> findAll();
+	
+	public void save(Cliente cliente);
+	
+	public Cliente findOne(long id);
+	
+	public void delete(long id);
+}
