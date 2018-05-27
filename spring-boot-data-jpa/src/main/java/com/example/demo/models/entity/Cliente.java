03c5650e3d0966d1932b3c43599d850a1cbafc0f
hector <hector.10.fisi@gmail.com>
2018-05-27 18:14:17 -0500

    creamos modulo factura (crear factura)

--- spring-boot-data-jpa/src/main/java/com/example/demo/models/entity/Cliente.java
+++ spring-boot-data-jpa/src/main/java/com/example/demo/models/entity/Cliente.java
@@ -1,13 +1,18 @@
 package com.example.demo.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 //import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
@@ -42,6 +47,12 @@ public class Cliente implements Serializable{
 	@DateTimeFormat(pattern="dd-MM-yyyy")
 	private Date createAt;
 	
+	@OneToMany(mappedBy="cliente",fetch = FetchType.LAZY, cascade= CascadeType.ALL)
+	private List<Factura> facturas;
+	
+	public Cliente(){
+		facturas = new ArrayList<Factura>();
+	}
 	private String foto;
 	
     public String getFoto() {
@@ -84,6 +95,14 @@ public class Cliente implements Serializable{
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
-
+	public List<Factura> getFacturas() {
+		return facturas;
+	}
+	public void setFacturas(List<Factura> facturas) {
+		this.facturas = facturas;
+	}
+    public void addFactura(Factura factura){
+    	facturas.add(factura);
+    }
     
 }
